95cf7f8bc05533ae76f279c61722510e1939f302
shrutiranawade <ranawadeshruti@gmail.com>
2023-02-11 23:41:03 +0530

    polymorphism assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+            //System.out.println(x*y);
+            return x*y;
+        }
+        public double product(double x, double y) {
+           // System.out.println(x*y);
+            return x*y;
+        }
+    }
+    public static void main(String[]args){
+        Product p = new Product();
+        p.product(2,6);
+        p.product(2.4,3.5);
+    }
 
 }
