f7fb180fcdb1a504249313627acdbb10acd4cf7e
shrutiranawade <ranawadeshruti@gmail.com>
2023-02-11 23:56:36 +0530

    polymorphism assignment1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,6 +6,10 @@ public class Main {
             //System.out.println(x*y);
             return x*y;
         }
+        public int product(int x, int y,int z) {
+            //System.out.println(x*y*z);
+            return x*y*z;
+        }
         public double product(double x, double y) {
            // System.out.println(x*y);
             return x*y;
@@ -15,6 +19,7 @@ public class Main {
         Product p = new Product();
         p.product(2,6);
         p.product(2.4,3.5);
+        p.product(2,3,6);
     }
 
 }
